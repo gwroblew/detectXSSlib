9e6aa875b3f1cbf71032c8844bd7657b8d162ff1
gwroblew <gwroblew@hotmail.com>
2013-06-28 17:07:46 -0700

    Update detectxsslib.c

--- detectxsslib/detectxsslib.c
+++ detectxsslib/detectxsslib.c
@@ -1,3 +1,12 @@
+//----------------------------------------------------------------------------------------
+// THIS CODE AND INFORMATION IS PROVIDED "AS-IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Greg Wroblewski. All rights reserved.
+//----------------------------------------------------------------------------------------
+
 #include "detectxsslib.h"
 
 #ifdef __GNUC__
