c7b73a99955d31b3579cb4dc5cbf7ad171b6aa75
gwroblew <gwroblew@hotmail.com>
2013-06-28 17:08:26 -0700

    Update xssscan.cpp

--- tools/xssscan/xssscan.cpp
+++ tools/xssscan/xssscan.cpp
@@ -1,3 +1,11 @@
+//----------------------------------------------------------------------------------------
+// THIS CODE AND INFORMATION IS PROVIDED "AS-IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Greg Wroblewski. All rights reserved.
+//----------------------------------------------------------------------------------------
 
 #include <stdio.h>
 #include <string.h>
